21bbabc06c6ea0f55265fc5d2e9d551c4a85067a
Robert Focke <robert_focke@yahoo.com>
2025-01-10 20:14:53 +0200

    setup django project

--- data/SOURCES.md
+++ data/SOURCES.md
@@ -1,1 +1,5 @@
+spcas.json
 https://nspca.co.za/wp-content/uploads/2024/12/2024-12-03-SPCAs-contact-list.pdf
+
+scraped pages
+- date 10 January 2024
---
+++ submission/spca/webapp/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Models
+
+class Province(models.TextChoices):
+    GAUTENG = 'GP', 'Gauteng'
+    WESTERN_CAPE = 'WC', 'Western Cape'
+    EASTERN_CAPE = 'EC', 'Eastern Cape'
+    KWAZULU_NATAL = 'KZN', 'KwaZulu-Natal'
+    FREE_STATE = 'FS', 'Free State'
+    LIMPOPO = 'LP', 'Limpopo'
+    MPUMALANGA = 'MP', 'Mpumalanga'
+    NORTHERN_CAPE = 'NC', 'Northern Cape'
+    NORTH_WEST = 'NW', 'North West'
+
+class SPCABranch(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=200)
+    address = models.TextField()
+    telephone = models.CharField(max_length=20)
+    emergency_telephone = models.CharField(max_length=20)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    province = models.CharField(max_length=3, choices=Province.choices)
+    website = models.URLField()
+
+class UserRole(models.TextChoices):
+    ADOPTER = 'adopter', 'Student'
+    SPCA_WORKER = 'spca_worker', 'Teacher'
